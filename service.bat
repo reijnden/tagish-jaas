c40e93ac8897c10ec7a1e62b0e6d3232b5820987
reijnden <reijnden@users.noreply.github.com>
2015-10-14 10:29:07 +0200

    Delete service.bat

--- service.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set HOME=D:\Works\NTSystem
-set PATH=%HOME%;%PATH%
-java -classpath C:\Jaas1.0\lib\jaas.jar;%HOME%;%CLASSPATH% com.tagish.auth.TestHarness > %HOME%\service.log 2> %HOME%\service.err
-
